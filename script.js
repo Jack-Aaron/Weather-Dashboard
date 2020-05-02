7f6679f0cbfadc7a43b744859c16b41ad27c99d7
Jonathan Alpart <panjialang@gmail.com>
2020-05-02 15:16:20 -0500

    primative button will return API info based on the city name in the input

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
 
-    
+   // var cityList = JSON.parse("src", "/city.list.min.json.gz");
+ //   console.log(cityList);
+
+    $("#search-button").on("click", function () {
+
+
+
+
+        var city = $("input").val();
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=18ac44d36d8e6681e3fb54132749a6ea";
+
+        $.ajax({
+            url: queryURL,
+            method: "get",
+        }).then((response) => console.log(response));
+    });
 });
