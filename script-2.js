31396549da37aad414f7b5990e18365a31cbacd6
Jonathan Alpart <panjialang@gmail.com>
2020-05-03 17:31:26 -0500

    i will instead convert what i have to vanilla js

---
+++ script-2.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+input = document.querySelector("input").value;
+var currentCity = document.querySelector("#current").value;
+
+var search = document.getElementById("search")
+
+search.addEventListener("click", function(event) {
+console.log(input);
+});
+
+// var today = moment.js stuff 
+
+console.log(input);
+console.log(currentCity);
+
+
+
+
+
+
+});
