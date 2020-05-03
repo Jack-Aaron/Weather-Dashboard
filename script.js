1761f0aa79b301a729b61568959909a59ae4ad55
Jonathan Alpart <panjialang@gmail.com>
2020-05-02 21:55:00 -0500

    current city clears and shows searched city on click

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-
-    // var cityList = JSON.parse("src", "/city.list.min.json.gz");
-    //   console.log(cityList);
-
-    $("#search-button").on("click", function () {
+    $("#search").on("click", function () {
         // gets what user has typed in the search input
         var city = $("input").val();
         // places it into the API call
@@ -16,8 +12,8 @@ $(document).ready(function () {
             // gets city name from API
             var city = response.name
             console.log(city);
-            //building the city history card
-            let cityHistory = $(`
+            // building the city history card
+            let cityHistoryCard = $(`
                 <button id ="city" style="visibility:hidden;">
                 <div class="card">
                 <h5 class="card-title">
@@ -26,7 +22,9 @@ $(document).ready(function () {
                 </div>
                 </button>
             `);
-            //building the current city card
+            // clear current history
+            $("#current").empty();
+            // building the current city card
             let currentCity = $(`
                 <div class="">
                 <h5>
@@ -35,13 +33,11 @@ $(document).ready(function () {
                 </div>
             `);
             // add the city card to history section
-            $("#city-history").prepend(cityHistory);
+            $("#city-history").prepend(cityHistoryCard);
             // add the city card to current section
             $("#current").prepend(currentCity);
             // add to localstorage
             // ...............
         });
-
-
     });
 });
