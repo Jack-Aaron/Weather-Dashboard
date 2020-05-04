e4de440c5e196f41d865a5b03ecba18f08789c89
Jonathan Alpart <panjialang@gmail.com>
2020-05-03 20:00:12 -0500

    onclick of search gets API responses based on prompt

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
-    var search = document.getElementById("search");
-    // when search button is clicked
-    search.addEventListener("click", function () {
-        // gets user input from search field
-        var input = document.querySelector("input").value;
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=18ac44d36d8e6681e3fb54132749a6ea";
-        $.ajax({
-            url: queryURL,
-            method: "get",
-        }).then(function (response) {
-            console.log(response); // DELETE LATER
-        });
-    });
+    var searchButton = document.getElementById("search");
+
+    searchButton.addEventListener("click", function (event) {
+
+        var typedInputInSearchBar = document.querySelector("input").value;
+
+        var partOfURLthatTakesUserInput = "?q=" + typedInputInSearchBar + "&appid=18ac44d36d8e6681e3fb54132749a6ea";
+
+        var kindsOfAPIcallsWeNeedToMake = ["weather", "forecast"];
+
+        queryURL(partOfURLthatTakesUserInput, kindsOfAPIcallsWeNeedToMake);
+
+        
+    })
+    function queryURL(partOfURLthatTakesUserInput, kindsOfAPIcallsWeNeedToMake) {
+        for (let i = 0; i < 2; i++) {
+            var querylURL = "http://api.openweathermap.org/data/2.5/" + kindsOfAPIcallsWeNeedToMake[i] + partOfURLthatTakesUserInput;
+            getResponse(querylURL);
+        }
+        function getResponse(queryURL) {
+            var response = $.ajax({
+                url: queryURL,
+                method: "get",
+            }).then(function (response) {
+                console.log(response);
+            });
+        }
+    }
 });
