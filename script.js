044b442cf2801505528ea2b2097e86346e877f76
Jonathan Alpart <panjialang@gmail.com>
2020-05-21 18:23:22 -0500

    UV color renders properly

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
     function send(data) {
         var weatherData = data[0];
         var forecastData = data[1];
-        uvQuery(uvBuilder(weatherData.coord));
+        uvBuilder(weatherData.coord);
         renderWeather(weatherData);
         forecast(forecastData);
         history(weatherData.name);
@@ -46,31 +46,10 @@ $(document).ready(function () {
         var p1 = "https://api.openweathermap.org/data/2.5/uvi";
         var p2 = "?" + apiKey;
         var p3 = "&lat=" + latLon.lat + "&lon=" + latLon.lon;
-        var weather = p1 + p2 + p3
-        var forecast = p1 + "/forecast" + p2 + p3 + "&cnt=" + 4;
-        var uvURLs = [weather, forecast];
-        return uvURLs;
-    }
-
-    function uvQuery(uvURLs) {
-        var responses = [];
-        var nUvURLS = uvURLs.length;
-        for (let n = 0; n < nUvURLS; n++) {
-            var queryURL = uvURLs[n];
-            fetch(queryURL)
-                .then(req => req.json())
-                .then(function (response) {
-                    responses.push(response);
-                    var nResponses = responses.length
-                    if (nResponses === nUvURLS) { uvSend(responses) }
-                });
-        }
-    }
-
-    function uvSend(data) {
-        var weatherUV = data[0];
-        var forecastUV = data[1];
-        renderWeatherUV(weatherUV);
+        var queryURL = p1 + p2 + p3
+        fetch(queryURL)
+            .then(req => req.json())
+            .then(function (response) { renderWeatherUV(response); });
     }
 
     function renderWeather(data) {
@@ -93,14 +72,15 @@ $(document).ready(function () {
 
     function renderWeatherUV(data) {
         var cityUV = document.createElement("h5");
-        cityUV.setAttribute("id", "UV");
+        cityUV.setAttribute("class", "UV");
         var currentUV = data.value;
         cityUV.textContent = currentUV;
+        console.log(currentUV);
         if (currentUV <= 3) { cityUV.setAttribute("style", "background-color:#299501"); }
-        if (3 <= currentUV < 6) { cityUV.setAttribute("style", "background-color:#F7E401"); }
-        if (6 <= currentUV < 8) { cityUV.setAttribute("style", "background-color:#F95901"); }
-        if (8 <= currentUV < 11) { cityUV.setAttribute("style", "background-color:#D90011"); }
-        if (currentUV >= 11) { cityUV.setAttribute("style", "background-color:#6C49CB"); }
+        else if (3 <= currentUV < 6) { cityUV.setAttribute("style", "background-color:#F7E401"); }
+        else if (6 <= currentUV < 8) { cityUV.setAttribute("style", "background-color:#F95901"); }
+        else if (8 <= currentUV < 11) { cityUV.setAttribute("style", "background-color:#D90011"); }
+        else if (currentUV >= 11) { cityUV.setAttribute("style", "background-color:#6C49CB"); }
         document.body.children[1].children[0].children[1].children[0].children[1].appendChild(cityUV);
     }
 
